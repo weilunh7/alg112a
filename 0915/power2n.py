3b88622111452892707a69a83a940b322411d26e
weilunh7 <99935037+weilunh7@users.noreply.github.com>
2023-09-28 09:44:30 +0800

    Update power2n.py

--- 0915/power2n.py
+++ 0915/power2n.py
@@ -40,3 +40,5 @@ print(power2n_a(5))
 print(power2n_b(5))
 print(power2n_c(5))
 print(power2n_d(5))
+
+#方法3 參考課程範例 https://github.com/ccc112a/py2cs/blob/master/02-%E6%BC%94%E7%AE%97%E6%B3%95/02-%E6%96%B9%E6%B3%95/01-%E6%9F%A5%E8%A1%A8%E6%B3%95/fiboanacci/fibonacci_lookup.py
